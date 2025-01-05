022bb99a461d43e15a2e64b87de164b1296d216e
Divyanshu Giri <154437803+divyannshugiri@users.noreply.github.com>
2025-01-05 18:09:29 +0000

    Commit Successful

---
+++ 01-Two_Sum/twosum.java
@@ -0,0 +1,18 @@
+ class solution
+ {
+    public int[] twosum(int[] nums, int target)
+    {
+        for(int i=0;i<=nums.length;i++)
+        {
+            int sum = 
+        }
+
+    
+    return nums;
+    }
+     public static void main(String[] args)
+     {
+        int[] nums={2,7,11,15};
+        int target =9;
+     }
+ }
